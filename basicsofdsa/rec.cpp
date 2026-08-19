0da6873ac97df7ee57300d6c2b05170496d068b2
Saksham-Sharma0712 <saksham.sh0712@gmail.com>
2026-08-19 23:33:55 +0530

    Add files via upload

--- basicsofdsa/rec.cpp
+++ basicsofdsa/rec.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-void f(int i, int n){
-    if(i>n){
+void f(int i, int sum){
+    if(i < 1){
+        cout<<sum;
         return;
     }
-    f(i,n-1);
-    cout<<n<<endl;
+    f(i-1,sum+i);
 }
 int main(){
     int n;
     cin>>n;
-    f(1,n);
+    f(n,0);
 }
