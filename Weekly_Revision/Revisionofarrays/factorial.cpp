20cceefe076ccc04b10dda8a567296ed93ee5c1c
Saksham-Sharma0712 <saksham.sh0712@gmail.com>
2026-08-20 23:04:28 +0530

    Implement factorial function in factorial.cpp
    Fix factorial calculation by adding increment in loop.

---
+++ Weekly_Revision/Revisionofarrays/factorial.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f(int n){
+    int fact = 1;
+    for(int i=1;i <= n;i++){
+        fact = fact * i;
+    }
+    cout<<fact;
+}
+int main(){
+    int n;
+    cin >> n;
+    f(n);
+}
